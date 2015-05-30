ec87fe1830446aece936fd2b9124134fd0de56f2
Austin Goudge <austin@opensceneryx.com>
2015-05-30 12:30:28 +0100

    Corrected paths in build scripts. Removed unnecessary plist entries from Mac build script. Updated copyright date in Mac build script.

--- buildmac.sh
+++ buildmac.sh
@@ -10,14 +10,12 @@ echo
 # Mac OS 10.4 and earlier: export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
 
-cd Builds\ -\ Installer.rbvcp/Mac\ OS\ X\ \(Intel\)/
+cd Builds\ -\ Installer.xojo_project/Mac\ OS\ X\ \(Cocoa\ Intel\)/
 
 # Code signing
-/usr/libexec/PlistBuddy -c "Delete :CFBundleIdentifier" OpenSceneryX\ Installer.app/Contents/Info.plist
 /usr/libexec/PlistBuddy -c "Delete :NSHumanReadableCopyright" OpenSceneryX\ Installer.app/Contents/Info.plist
 /usr/libexec/PlistBuddy -c "Delete :LSApplicationCategoryType" OpenSceneryX\ Installer.app/Contents/Info.plist
-/usr/libexec/PlistBuddy -c "Add :CFBundleIdentifier string 'com.AustinGoudge.OpenSceneryX'" OpenSceneryX\ Installer.app/Contents/Info.plist
-/usr/libexec/PlistBuddy -c "Add :NSHumanReadableCopyright string '© 2012, Austin Goudge'" OpenSceneryX\ Installer.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Add :NSHumanReadableCopyright string '© 2015, Austin Goudge'" OpenSceneryX\ Installer.app/Contents/Info.plist
 /usr/libexec/PlistBuddy -c "Add :LSApplicationCategoryType string 'public.app-category.simulation-games'" OpenSceneryX\ Installer.app/Contents/Info.plist
 codesign --signature-size 6400 -f -v -s 'Austin Goudge' OpenSceneryX\ Installer.app
 
