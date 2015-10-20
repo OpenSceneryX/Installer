86939778a27ebf6cec1975c956e0a60f39f6a93c
Austin <austin.goudge@psycle.com>
2015-10-20 14:10:15 +0100

    Updated Linux build script to include all three versions. Tidied Mac and Windows build scripts in line with Linux script

--- buildlinux.sh
+++ buildlinux.sh
@@ -1,14 +1,35 @@
 #!/bin/sh
 
 echo
-echo ------------------------
-echo Compressing Linux Installer
-echo ------------------------
-echo
+echo "==========="
+echo "Linux Build"
+echo "==========="
 
 # Create a tarball of OpenSceneryX Installer but exclude resource forks
 # Mac OS 10.4 and earlier: export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
 
+echo
+echo "x86 32-bit"
+echo "----------"
+
 cd Builds\ -\ Installer.xojo_project/Linux/
-zip -r ../../OpenSceneryX-Installer-Linux.zip OpenSceneryX\ Installer
+zip -r ../../OpenSceneryX-Installer-Linux-x86-32.zip OpenSceneryX\ Installer
+cd ../../
+
+echo
+echo "x86 64-bit"
+echo "----------"
+
+cd Builds\ -\ Installer.xojo_project/Linux\ 64\ bit/
+zip -r ../../OpenSceneryX-Installer-Linux-x86-64.zip OpenSceneryX\ Installer
+cd ../../
+
+echo
+echo "ARM 32-bit"
+echo "----------"
+
+cd Builds\ -\ Installer.xojo_project/Linux\ ARM/
+zip -r ../../OpenSceneryX-Installer-Linux-ARM-32.zip OpenSceneryX\ Installer
+cd ../../
+
