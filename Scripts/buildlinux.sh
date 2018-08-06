adb734aa391f9d8b44bf43e6747b59abd191f645
aussig <austin@opensceneryx.com>
2018-08-06 14:08:21 +0100

    Move build scripts into Scripts

--- Scripts/buildlinux.sh
+++ Scripts/buildlinux.sh
@@ -8,6 +8,7 @@ echo "==========="
 # Create a tarball of OpenSceneryX Installer but exclude resource forks
 # Mac OS 10.4 and earlier: export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
+cd ..
 
 echo
 echo "x86 32-bit"
--- Scripts/buildmac.sh
+++ Scripts/buildmac.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 echo
-echo "============="
-echo "Windows Build"
-echo "============="
+echo "========="
+echo "Mac Build"
+echo "========="
 
 # Create a zip of OpenSceneryX Installer but exclude resource forks
 # Mac OS 10.4 and earlier: export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
+cd ..
 
-cd Builds\ -\ Installer.xojo_project/Windows\ 64\ bit/
+cd Builds\ -\ Installer.xojo_project/OS\ X\ 64\ bit
 
 echo
 echo "Zipping"
 echo "-------"
-zip -r ../../OpenSceneryX-Installer-Windows.zip OpenSceneryX\ Installer
-
+zip -r ../../OpenSceneryX-Installer-Mac.zip OpenSceneryX\ Installer.app
